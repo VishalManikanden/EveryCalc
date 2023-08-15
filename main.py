9db7dfa176b0fcd0d3bb2eaf8db57c0da91a64da
VishalManikanden <vishy.mani@gmail.com>
2023-08-15 16:50:57 -0400

    Create tables

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.app_context().push()
 load_dotenv(find_dotenv())
 app.config['SECRET_KEY'] = os.getenv("SecretKey")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL2", "sqlite:///EveryCalc.db")
-DATABASE_URL = os.environ['DATABASE_URL']
+DATABASE_URL = os.environ['DATABASE_URL2']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
